eef1bc490aee5de681f42119a86442a600309573
meddhawi <meddhawi@gmail.com>
2022-03-06 11:04:22 +0700

    added authorize for specific user and precaution although the usage is rather questionable

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const api = require('../controllers/apiController')
+const api = require('../controllers/apiController');
+const authorize = require('../middlewares/authorize');
 const restrict = require('../middlewares/restrict')
+
 
 router.post('/register', api.register)
 router.post('/login', api.login)
 
 router.get('/me', restrict, api.currentProfile)
 router.get('/all', api.showAllProfile)
-router.post('/me/update', restrict, api.updateProfile)
+router.post('/me/update', restrict, authorize, api.updateProfile)
 router.post('/score', restrict, api.updateDetails)
 
 
